3a3e5442e76fc34b7250acc4c6d3654100e3b6a9
michaelgavin <michael.a.gavin@gmail.com>
2015-07-08 10:40:40 -0400

    updated DESCRIPTION file (author and maintainer). simplified aliases

--- R/aliases.R
+++ R/aliases.R
@@ -1,19 +1,19 @@
-getTexts = function(dl) {
+texts = function(dl) {
   dt = getDocTexts(dl)
   return(dt)
 }
 
-getFrequencies = function(dt) {
+frequencies = function(dt) {
   df = getDocFrequencies(dt)
   return(df)
 }
 
-getConcordance = function(dt, term, context) {
+concordance = function(dt, term, context) {
   dc = getDocConcordance(dt, term, context)
   return(dc)
 }
 
-getAssociations = function(dc, df) {
+associations = function(dc, df) {
   da = getDocAssociations(dc, df)
   return(da)
 }
